40c68df2cc5ec5ab27b92e6cd8c706e956fc52c0
Nasif-Imtiaj <nasifimtiaj.16@gmail.com>
2021-03-13 01:16:45 +0600

    Displayed better info on page

--- center/views.py
+++ center/views.py
@@ -45,6 +45,14 @@ class AuctionTableDetailView(DetailView):
     template_name = 'center/detail_product.html'
     model = AuctionItem
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'create_product_nav': 'active',
+            'bets': Bets.objects.filter(item=self.object.id)
+        })
+        return context
+
 
 
 class AuctionTableCreateView(LoginRequiredMixin, CreateView):
